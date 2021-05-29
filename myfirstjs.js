3e13093b95e18a51ff9e7f1b1bcb7d86d813a68e
sravaneravelli <sravaneravelli@gmail.com>
2021-05-29 14:22:20 +0530

    day one half task done

--- myfirstjs.js
+++ myfirstjs.js
@@ -1,1 +1,36 @@
-console.log("hello world")
+console.log("intial project");
+var a=3;
+var b=3;
+var c=a+b;
+console.log(c);
+var d=a-b;
+console.log(d);
+var e=a*b;
+console.log(e);
+var f=a/b;
+console.log(f);
+console.log("multiple of 3 numbers");
+var u=7;
+var v=8;
+var w=9;
+var k=u*v*w;
+console.log(k);
+console.log("print my name");
+var name="sravan";
+console.log("my name is"+name);
+console.log("add to strings");
+var string1="system";
+var string2="updated";
+var string3=string1+" is "+string2;
+console.log(string3);
+console.log("for loops");
+//print 10 table starting with 12 and ends with 1
+for (var i=12;i>=1;i--)
+{
+    console.log("10"+"X"+i+"="+i*10);
+}
+//print multiples of 3 upto 10
+for(var i=3;i<=10;i=i+3)
+{
+    console.log(i)
+}
