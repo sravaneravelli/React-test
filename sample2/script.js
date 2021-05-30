57053fc5e3b53a9f5f53bc4a21f51b4194c69a2e
sravaneravelli <sravaneravelli@gmail.com>
2021-05-30 11:57:11 +0530

    morning update done

---
+++ sample2/script.js
@@ -0,0 +1,17 @@
+function add(a,b){
+
+    console.log(a+b)
+    }
+    function minus(a,b){
+        console.log(a-b)
+    }
+    function multipication(a,b){
+        console.log(a*b)
+    }
+    function division(a,b){
+        console.log(a/b)
+    }
+    function exponent(a,b){
+        console.log(a**b)
+    }
+    
